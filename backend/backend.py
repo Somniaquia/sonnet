31de7436bb562219d82be8701c5285c18bf07283
welidom <khs060222@gmail.com>
2023-11-04 10:00:30 +0900

    intermediate commit

--- backend/backend.py
+++ backend/backend.py
@@ -4,15 +4,13 @@ import json
 import asyncio
 import websockets
 
-async def echo(websocket, path):
-    async for message in websocket:
-        print(f"Received message: {message}")
-
-        Agent.prompt(json.message)
-
-        await websocket.send(f"Echo: {message}")
-
-start_server = websockets.serve(echo, "localhost", 3000)
+async def accept(websocket, path):
+    while 1:
+        data = await websocket.recv()
+        data = json.loads(data)
+        if data['type'] == 'msg':
+            print(data['context'])
+start_server = websockets.serve(accept, "localhost", 3000)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
