ca672e2a948f102eae946ca1d23fc0ae6da3b8a6
Somniaquia <Somniaquia@gmail.com>
2023-11-04 10:33:44 +0900

    a

--- backend/backend.py
+++ backend/backend.py
@@ -4,6 +4,17 @@ import json
 import asyncio
 import websockets
 
+async def echo(websocket, path):
+    async for message in websocket:
+        print(f"Backend received a message: {message}")
+        response = "fallback"
+        try:
+            response = Agent.prompt(json.loads(message)['message'])
+        except json.JSONDecodeError as e:
+            print(e)
+
+        await websocket.send(json.dumps({"response": response}))
+
 async def accept(websocket, path):
     while 1:
         data = await websocket.recv()
@@ -11,7 +22,7 @@ async def accept(websocket, path):
         if data['type'] == 'msg':
             print(data['context'])
             
-start_server = websockets.serve(accept, "localhost", 3000)
+start_server = websockets.serve(echo, "localhost", 3000)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
