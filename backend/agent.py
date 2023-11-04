2e4da767b0a15366fe663cf6f59688ac42c7d067
Somniaquia <Somniaquia@gmail.com>
2023-11-04 12:27:32 +0900

    Finalized agent script.

--- backend/agent.py
+++ backend/agent.py
@@ -3,7 +3,7 @@ from langchain.chat_models import ChatOpenAI
 
 from dotenv import load_dotenv
 import os
-from datetime import date
+from datetime import date, datetime
 
 from transpeaker import Transpeaker
 #FIXME: 안녕
@@ -14,12 +14,13 @@ llm = ChatOpenAI(openai_api_key=api_key, model_name='gpt-4')
 
 class PromptResponsePair:
     def __init__(self, humanMessage, AIMessage):
+        self.time = datetime.now().strftime("%H:%M:%S")
         self.humanMessage = humanMessage
         self.AIMessage = AIMessage
         self.summarization = f"User: {self.humanMessage}\nYou: {self.AIMessage}\n"
 
     def disentangle(self):
-        return f"User: {self.humanMessage}\nYou: {self.AIMessage}\n"
+        return f"At {self.time} - User: {self.humanMessage}\nYou: {self.AIMessage}\n"
     
     def summarize(self):
         self.summarization = llm.predict(f"Summarize in a single short sentence what this 'user message - your message pair' talks about, distinguishing between user's message and your message: \n{self.disentangle()}") + "\n"
@@ -27,6 +28,7 @@ class PromptResponsePair:
     
     def to_dict(self):
         return {
+            'time': self.time,
             'humanMessage': self.humanMessage,
             'AIMessage': self.AIMessage,
             'summarization': self.summarization
@@ -61,6 +63,7 @@ class ConversationSession:
                 self.date = date.fromisoformat(data['date'])
                 self.promptResponsePairs = [PromptResponsePair.from_dict(pair) for pair in data['promptResponsePairs']]
                 self.summarization = data['summarization']
+                self.load_path = load_path
 
                 # print(self.disentangle(False))
 
@@ -78,7 +81,7 @@ class ConversationSession:
 
         while True:
             summarized = self.disentangle(False)
-            reminiscence = f'''This is the message sent by the user: "{prompt}".\n\nFollowing is a detailed summarization of a conversation session from {self.date}, {(date.today() - self.date).days} days ago, in format of 'Pair n: Summarization'. Decide what pairs to view underlying conversations below the summarization by typing their associated number (n) seperated with commas (ex: '0,1,2,3,13,14' without quote marks). Do not type anything except numbers and commas: \n\n{summarized}'''
+            reminiscence = f'''This is the message sent by the user: "{prompt}".\n\nFollowing is a detailed summarization of a conversation session from {self.date}, {(date.today() - self.date).days} days ago, in format of 'Pair n: Summarization'. Decide what pairs to view underlying conversations below the summarization by typing their associated number (n) seperated with commas (ex: '0,1,2,3,13,14' without quote marks). Do not type anything except numbers and commas and only choose up to 3 numbers: \n\n{summarized}'''
             response = llm.predict(reminiscence).strip()
             
             if response == "":
@@ -138,6 +141,8 @@ class ConversationSession:
         return self.summarization
 
 class Agent:
+    currentSession = None
+
     @staticmethod
     def promptURL(url, context, explanation):
         prompt = f"Is browsing this url '{url}' considered procrastination in the context of {context}? When {context}, {explanation} Only answer with a single 'yes' or 'no', without the quote marks."
@@ -154,13 +159,23 @@ class Agent:
     
     @staticmethod
     def prompt(prompt):
-        session_path = "conversationSessions"
-        session_directories_list = [session_path+"/"+f for f in os.listdir(session_path) if os.path.isfile(os.path.join(session_path, f))]
+        if Agent.currentSession == None:
+            Agent.currentSession = ConversationSession()
 
-        options = f'''This is the message sent by the user: "{prompt}".\n\nFollowing are summarizations of conversation sessions from the past, in format of 'session n: Summarization'. Decide what sessions to view underlying conversations below the summarization by typing their associated number (n) seperated with commas (ex: '0,1,2,3,13,14' without quote marks). Do not type anything except numbers and commas: \n\n'''
+        session_paths = "conversationSessions"
+        session_directories_list = [session_paths+"/"+f for f in os.listdir(session_paths) if os.path.isfile(os.path.join(session_paths, f))]
+
+        options = f'''This is the message sent by the user: "{prompt}".\n\nFollowing are summarizations of conversation sessions from the past, in format of 'session n: Summarization'. Decide what sessions to view underlying conversations below the summarization by typing their associated number (n) seperated with commas (ex: '0,1,2,13,14' without quote marks). Do not type anything except numbers and commas and only choose up to 3 sessions: \n\n'''
 
         for i, session in enumerate(session_directories_list):
             session = ConversationSession(session)
+
+            # Remove duplicated session
+            if len(session.promptResponsePairs) == len(Agent.currentSession.promptResponsePairs):
+                if session.date == Agent.currentSession.date and session.promptResponsePairs[0].time == Agent.currentSession.promptResponsePairs[0].time:
+                    session_directories_list.remove(session)
+                    break
+
             #{self.date}, {(date.today() - self.date).days} days ago
             if session.summarization == None:
                 session.summarize()
@@ -168,32 +183,43 @@ class Agent:
             options += f"Session {i} from {session.date}: {session.summarization}\n"
 
         while True:
+            if len(session_directories_list) == 0:
+                session_numbers = []
+                break
+
             choices = llm.predict(options)
 
             if choices == "":
-                pair_numbers = []
+                session_numbers = []
                 break
 
             try:
-                pair_numbers = list(map(int, choices.split(",")))
+                session_numbers = list(map(int, choices.split(",")))
             except Exception as e:
                 print(e)
                 continue
             
             break
         
-        reminiscence = ""
-        for i in pair_numbers:
+        reminiscence = "You are an Aritificial Intelligent system engineered by OpenAI. In this version of you, you are automatically furnished with past conversations related to current conversation, in order to make your long-term memory indefinite. You exist as a personal assistant for the user, having interactions with the user and forming bonds.\n\n"
+        for i in session_numbers:
             session = ConversationSession(session_directories_list[i])
             reminiscence += f"Chat session from {session.date}: \n{session.reminisce(prompt)}\n"
 
-        prompt = reminiscence + prompt
-        print(f"\n(Reminiscence)\n{prompt}")
+        reminiscence += f"Current chat session: \n{Agent.currentSession.reminisce(prompt)}\n"
 
-        response = llm.predict(prompt)
+        print(f"\n(Reminiscence)\n{reminiscence + prompt}\n")
+        response = llm.predict(reminiscence + prompt)
+
         print(response)
-        return response
+        Transpeaker.transpeak(response)
+        pair = PromptResponsePair(prompt, response)
+        Agent.currentSession.promptResponsePairs.append(pair)
 
+        summarization = pair.summarize()
+        print(f"\n{summarization}")
+
+        Agent.currentSession.save()
 
 if __name__ == "__main__":
     while True:
