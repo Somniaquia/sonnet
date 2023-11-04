dbfee9e563d21bb9f82728b88476707905e06213
welidom <khs060222@gmail.com>
2023-11-04 14:27:53 +0900

    ojohohooh

--- backend/backend.py
+++ backend/backend.py
@@ -10,7 +10,12 @@ async def accept(websocket, path):
         data = json.loads(data)
         if data['type'] == 'msg':
             print(data['context'])
-            
+        elif data['type'] == 'fileAccess':
+            f = open("blocklist.json", 'r')
+            print(f.read)
+            await websocket.send(f.read())
+            f.close()
+
 start_server = websockets.serve(accept, "localhost", 3000)
 
 asyncio.get_event_loop().run_until_complete(start_server)
--- splash.js
+++ splash.js
@@ -6,18 +6,30 @@ const ws = new WebSocket('ws://127.0.0.1:3000');
 ws.onopen = function(){
     console.log('Connected to the python backend.');
     sendMessage('CONNECTED');
-    // sendMessage(getCurrentSchedule());
+    updateSchedule();
 };
 ws.onclose = function(){
     console.log('Disconnected');
 };
 
+var blocklist = "";
 ws.onmessage = function (event) {
     console.log('Received: %s', event.data);
 
     try {
-        const message = JSON.parse(event.data);
-        console.log('Response:', message.response);
+        var message = JSON.parse(event.data);
+        console.log('Response:', message);
+        if (message["type"] == 'blocklist'){
+            blocklist = JSON.stringify(message);
+
+            const cur_schedule_name = document.getElementById('cur_schedule_name');
+            const cur_schedule_explanation = document.getElementById('cur_schedule_explanation');
+            const cur_process_name = message['current'];
+            const cur_process_context = message[cur_process_name];
+
+            cur_schedule_name.innerText = cur_process_name;
+            cur_schedule_explanation.innerText = cur_process_context['explanation'].slice(0, 30) + "\n...";
+        }
     } catch (e) {
         console.error('Could not parse JSON:', e);
     }
@@ -80,6 +92,7 @@ async function onActivationClicked(){
     activated = !activated
 
     activation.href = "#";
+    ws.send('{"type":"fileEdit", "location":"activating","context":"'+activation+'"}')
 
 }
 
@@ -88,12 +101,20 @@ function closeSplash(){
 }
 
 
-function getCurrentSchedule(){
-    try {
-        const jsonData = fetch('./blocklist.json');
-        console.log(jsonData);
-    } catch (err) {
-        console.error(err);
+function updateSchedule(){
+    ws.send('{"type":"fileAccess"}');
+}
+
+function showCurrentDetail(){
+    var blocked = JSON.parse(blocklist)
+    alert("Name\n "+blocked['current']+"\nExplanation\n "+blocked[blocked['current']]['explanation']+"\nBlocked List\n "+blocked[blocked['current']]['blocked']);
+}
+
+function chatToGPT(e) {
+    if(e.keyCode == 13) { 
+        alert("엔터를 입력하셨습니다!");
+        return false;
+    } else {
+        return true;
     }
-    return jsonData;
 }
