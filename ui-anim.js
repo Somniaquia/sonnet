f2c958189cf76c3782a89eca585b5dcc311b77a7
kimdoyun0208 <kdyandy0208@gmail.com>
2023-12-04 15:17:35 +0900

    make ui

---
+++ ui-anim.js
@@ -0,0 +1,15 @@
+function openLeftWindow() {
+    document.getElementById("left_window").style.transform = 'translateX(0px)';
+}
+
+function closeLeftWindow() {
+    document.getElementById("left_window").style.transform = 'translateX(-650px)';
+}
+
+function openRightWindow() {
+    document.getElementById("right_window").style.transform = 'translateX(0px)';
+}
+
+function closeRightWindow() {
+    document.getElementById("right_window").style.transform = 'translateX(650px)';
+}
