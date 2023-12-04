7f1367bfff92473f501d257476bb3294e198269b
Somniaquia <Somniaquia@gmail.com>
2023-12-04 13:09:56 +0900

    Removed splash screen in order to revamp it

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow, globalShortcut, session, ipcMain } = electron;
 const { spawn } = require('child_process');
 
 var canvasWindow = null;
-var splashWindow = null;
+var uiWindow = null;
 
 function createCanvasWindow() {
     const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
@@ -32,30 +32,30 @@ function createCanvasWindow() {
 }
 
 let showInterval, hideInterval;
-let splashWindowActivated = false;
+let uiWindowActivated = false;
 
-function createOrToggleSplashWindow() {
+function createOrToggleuiWindow() {
 
     function driftWindowToShow() {
         clearInterval(showInterval);
         clearInterval(hideInterval);
 
         const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
-        let x = splashWindow.getPosition()[0];
+        let x = uiWindow.getPosition()[0];
         
         showInterval = setInterval(() => {
             // canvasWindow.webContents.executeJavaScript('canvasApp()');
 
             if (x < 0) {
                 x += 10;
-                splashWindow.setPosition(x, 0, false);
-                splashWindow.setSize(width / 3, height, false);
+                uiWindow.setPosition(x, 0, false);
+                uiWindow.setSize(width / 3, height, false);
             } else {
-                splashWindowActivated = true;
-                splashWindow.setPosition(0, 0, false);
-                splashWindow.setSize(Math.round(width / 3), height, false);
+                uiWindowActivated = true;
+                uiWindow.setPosition(0, 0, false);
+                uiWindow.setSize(Math.round(width / 3), height, false);
                 clearInterval(showInterval);
-                splashWindow.focus();
+                uiWindow.focus();
             }
         }, 1);
     }
@@ -65,31 +65,31 @@ function createOrToggleSplashWindow() {
         clearInterval(hideInterval);
 
         const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
-        let x = splashWindow.getPosition()[0];
+        let x = uiWindow.getPosition()[0];
         
         hideInterval = setInterval(() => {
             if (x >= -width / 3) {
                 x -= 10;
-                splashWindow.setPosition(x, 0, false);
-                splashWindow.setSize(Math.round(width / 3), height, false);
+                uiWindow.setPosition(x, 0, false);
+                uiWindow.setSize(Math.round(width / 3), height, false);
             } else {
-                splashWindowActivated = false;
-                splashWindow.setPosition(-Math.round(width / 3), 0, false);
-                splashWindow.setSize(Math.round(width / 3), height, false);
+                uiWindowActivated = false;
+                uiWindow.setPosition(-Math.round(width / 3), 0, false);
+                uiWindow.setSize(Math.round(width / 3), height, false);
                 clearInterval(hideInterval);
             }
         }, 1);
     }
 
-    if (splashWindow && !splashWindow.isDestroyed()) {
-        if (splashWindowActivated) {
+    if (uiWindow && !uiWindow.isDestroyed()) {
+        if (uiWindowActivated) {
             driftWindowToHide();
         } else {
             driftWindowToShow();
         }
     } else {
         const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
-        splashWindow = new BrowserWindow({
+        uiWindow = new BrowserWindow({
             x: 0,
             y: 0,
             width: width / 3,
@@ -109,14 +109,14 @@ function createOrToggleSplashWindow() {
             }
         });
         
-        splashWindow.loadFile('splash.html');
-        splashWindow.once('ready-to-show', () => {
-            splashWindow.setHasShadow(false);
-            splashWindow.show();
+        uiWindow.loadFile('ui.html');
+        uiWindow.once('ready-to-show', () => {
+            uiWindow.setHasShadow(false);
+            uiWindow.show();
             driftWindowToShow();
         });
         
-        splashWindow.on('blur', () => {
+        uiWindow.on('blur', () => {
             driftWindowToHide();
         });
     }
@@ -124,15 +124,15 @@ function createOrToggleSplashWindow() {
 
 app.whenReady().then(() => {
     createCanvasWindow();
-    globalShortcut.register('CommandOrControl+Alt+P', createOrToggleSplashWindow);
+    globalShortcut.register('CommandOrControl+Alt+P', createOrToggleuiWindow);
 
     console.log(canvasWindow.y)
-    const blockerProcess = spawn('C:\\Users\\Somni\\anaconda3\\envs\\torch\\python.exe', ['-u', 'backend\\blocker.py'], { studio: 'pipe' });
+    // const blockerProcess = spawn('C:\\Users\\Somni\\anaconda3\\envs\\torch\\python.exe', ['-u', 'backend\\blocker.py'], { studio: 'pipe' });
     // const blockerProcess = spawn('/usr/bin/python3', ['-u', 'backend/blocker.py'], { studio: 'pipe' });
     blockerProcess.stdout.on('data', (data) => { console.log(`${data.slice(0, -1)}`); });
     blockerProcess.stderr.on('data', (data) => { console.log(`${data.slice(0, -1)}`); });
 
-    const transcriptorProcess = spawn('C:\\Users\\Somni\\anaconda3\\envs\\torch\\python.exe', ['-u', 'backend\\backend.py'], { studio: 'pipe' });
+    // const transcriptorProcess = spawn('C:\\Users\\Somni\\anaconda3\\envs\\torch\\python.exe', ['-u', 'backend\\backend.py'], { studio: 'pipe' });
     // const transcriptorProcess = spawn('/usr/bin/python3', ['-u', 'backend/backend.py'], { studio: 'pipe' });
     transcriptorProcess.stdout.on('data', (data) => { console.log(`${data.slice(0, -1)}`); });
     transcriptorProcess.stderr.on('data', (data) => { console.log(`${data.slice(0, -1)}`); });
